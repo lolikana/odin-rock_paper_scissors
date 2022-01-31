5aee56b7881d93474f1580ae71970c417e03087e
lolikana <thomas.guiart@gmail.com>
2022-01-31 14:55:15 +0900

    add return score

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const choiceArray = ["rock", "paper", "scissors", "lizard", "spock"];
 
-function game() {
-
-    function playerPlay() {
-      return choiceArray[Math.floor(Math.random() * choiceArray.length)];
-    }
-    const playerSelection = playerPlay();
-    console.log("Player: " + playerSelection);
+function playerPlay() {
+  return choiceArray[Math.floor(Math.random() * choiceArray.length)];
+}
 
   /*
     function playerPlay() {
@@ -20,35 +16,64 @@ function game() {
     console.log("Player: " + playerSelection)
   */
 
-    function computerPlay() {
-      return choiceArray[Math.floor(Math.random() * choiceArray.length)];
-    }
-    const computerSelection = computerPlay();
-    console.log("Computer: " + computerSelection);
-
-    function playRound(playerSelection, computerSelection) {
-      let resultBattle;
-        if (playerSelection === "rock" && (computerSelection === "lizard" || computerSelection === "scissors")) {
-          resultBattle = "Player Win";
-        } else if (playerSelection === "paper" && (computerSelection === "rock" || computerSelection === "spock")) {
-          resultBattle = "Player Win";
-        } else if (playerSelection === "scissors" && (computerSelection === "paper" || computerSelection === "lizard")) {
-          resultBattle = "Player Win";
-        } else if (playerSelection === "lizard" && (computerSelection === "paper" || computerSelection === "spock")) {
-          resultBattle = "Player Win";
-        } else if (playerSelection === "spock" && (computerSelection === "scissors" || computerSelection === "rock")) {
-          resultBattle = "Player Win";
-        } else if (playerSelection === computerSelection) {
-          resultBattle = "Exaequo! That was a close one";
-        } else {
-          resultBattle = "Computer Win";
-        };
-     return resultBattle;
-   }
-   console.log(playRound(playerSelection, computerSelection));
+function computerPlay() {
+  return choiceArray[Math.floor(Math.random() * choiceArray.length)];
 }
 
+let resultBattle;
+const scorePlayerArray = [];
+const scoreComputerArray = [];
+const scoreTie = [];
+
+function game() {
+  const playerSelection = playerPlay();
+  console.log("Player: " + playerSelection);
+
+  const computerSelection = computerPlay();
+  console.log("Computer: " + computerSelection);
+
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === "rock" && (computerSelection === "lizard" || computerSelection === "scissors")) {
+          resultBattle = "Player Win";
+          scorePlayerArray.push("1");
+        } else if (playerSelection === "paper" && (computerSelection === "rock" || computerSelection === "spock")) {
+          resultBattle = "Player Win";
+          scorePlayerArray.push("1");
+        } else if (playerSelection === "scissors" && (computerSelection === "paper" || computerSelection === "lizard")) {
+          resultBattle = "Player Win";
+          scorePlayerArray.push("1");
+        } else if (playerSelection === "lizard" && (computerSelection === "paper" || computerSelection === "spock")) {
+          resultBattle = "Player Win";
+          scorePlayerArray.push("1");
+        } else if (playerSelection === "spock" && (computerSelection === "scissors" || computerSelection === "rock")) {
+          resultBattle = "Player Win";
+          scorePlayerArray.push("1");
+        } else if (playerSelection === computerSelection) {
+          resultBattle = "Exaequo! That was a close one";
+          scoreTie.push("1");
+        } else {
+          resultBattle = "Computer Win";
+          scoreComputerArray.push("1");
+        };
+     return resultBattle;
+     return scorePlayerArray;
+     return scoreComputerArray;
+     return scoreTie;
+   };
+   console.log(playRound(playerSelection, computerSelection));
+   console.log("Player score: "+ scorePlayerArray.length);
+   console.log("Computer score: " + scoreComputerArray.length);
+   console.log("Tie score: " + scoreTie.length);
+}
 
 for (let i = 0; i < 5; i++) {
   game();
+}
+
+if (scorePlayerArray.length < scoreComputerArray.length) {
+  alert("Computer won");
+} else if (scorePlayerArray.length > scoreComputerArray.length) {
+  alert("Player won" );
+} else {
+  alert("Exaequo");
 }
