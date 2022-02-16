984fa43b19e77c8207a6292690c4edc15d9531e5
lolikana <thomas.guiart@gmail.com>
2022-02-16 15:51:20 +0900

    press button call game, change alert

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const choiceArray = ["rock", "paper", "scissors", "lizard", "spock"];
 
 
-  const rockBtn = document.getElementsByClassName("rock");
+  const rockBtn = document.getElementsByClassName("rock")[0];
   const paperBtn = document.getElementsByClassName("paper");
   const scissorsBtn = document.getElementsByClassName("scissors");
   const lizardBtn = document.getElementsByClassName("lizard");
   const spockBtn = document.getElementsByClassName("spock");
+
+  rockBtn.addEventListener("click", game);
 
 
   function playerPlay() {
@@ -43,27 +45,26 @@ function game() {
   console.log("Computer: " + computerSelection);
 
     function playRound(playerSelection, computerSelection) {
-        if (playerSelection === "rock" && (computerSelection === "lizard" || computerSelection === "scissors")) {
+        if ((playerSelection === "rock" && (computerSelection === "lizard" || computerSelection === "scissors"))
+          ||
+          (playerSelection === "paper" && (computerSelection === "rock" || computerSelection === "spock"))
+          ||
+          (playerSelection === "scissors" && (computerSelection === "paper" || computerSelection === "lizard"))
+          ||
+          (playerSelection === "lizard" && (computerSelection === "paper" || computerSelection === "spock"))
+          ||
+          (playerSelection === "spock" && (computerSelection === "scissors" || computerSelection === "rock"))){
           resultBattle = "Player Win";
           scorePlayerArray.push("1");
-        } else if (playerSelection === "paper" && (computerSelection === "rock" || computerSelection === "spock")) {
-          resultBattle = "Player Win";
-          scorePlayerArray.push("1");
-        } else if (playerSelection === "scissors" && (computerSelection === "paper" || computerSelection === "lizard")) {
-          resultBattle = "Player Win";
-          scorePlayerArray.push("1");
-        } else if (playerSelection === "lizard" && (computerSelection === "paper" || computerSelection === "spock")) {
-          resultBattle = "Player Win";
-          scorePlayerArray.push("1");
-        } else if (playerSelection === "spock" && (computerSelection === "scissors" || computerSelection === "rock")) {
-          resultBattle = "Player Win";
-          scorePlayerArray.push("1");
+          alert("Player won");
         } else if (playerSelection === computerSelection) {
           resultBattle = "Exaequo! That was a close one";
           scoreTie.push("1");
+          alert("Exaequo");
         } else {
           resultBattle = "Computer Win";
           scoreComputerArray.push("1");
+          alert("Compuer won");
         };
      return resultBattle;
      return scorePlayerArray;
@@ -76,23 +77,15 @@ function game() {
    console.log("Player score: "+ scorePlayerArray.length);
    console.log("Computer score: " + scoreComputerArray.length);
    console.log("Tie score: " + scoreTie.length);
-}
 
+   console.log(scorePlayerArray);
+   console.log(scoreComputerArray);
+   document.getElementById("score_player").innerHTML = scorePlayerArray.length;
+   document.getElementById("score_bot").innerHTML = scoreComputerArray.length;
+}
+/*
 //5 games in a row
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 10; i++) {
   game();
 }
-
-
-console.log(scorePlayerArray);
-console.log(scoreComputerArray);
-document.getElementById("score_player").innerHTML = scorePlayerArray.length;
-document.getElementById("score_bot").innerHTML = scoreComputerArray.length;
-
-if (scorePlayerArray.length < scoreComputerArray.length) {
-  alert("Computer won");
-} else if (scorePlayerArray.length > scoreComputerArray.length) {
-  alert("Player won" );
-} else {
-  alert("Exaequo");
-}
+*/
