a4fa31e5a10105734332161462cd90054cab3886
lolikana <thomas.guiart@gmail.com>
2022-02-17 11:03:32 +0900

    addEventListener player selection

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const choiceArray = ["rock", "paper", "scissors", "lizard", "spock"];
 
-
-  const rockBtn = document.getElementsByClassName("rock")[0];
-  const paperBtn = document.getElementsByClassName("paper");
-  const scissorsBtn = document.getElementsByClassName("scissors");
-  const lizardBtn = document.getElementsByClassName("lizard");
-  const spockBtn = document.getElementsByClassName("spock");
-
-  rockBtn.addEventListener("click", game);
-
-
-  function playerPlay() {
-    return choiceArray[Math.floor(Math.random() * choiceArray.length)];
-  }
-
+const pressButton = document.getElementsByClassName("buttons_player");
+const pressButtonBot = document.getElementsByClassName("buttons_bot");
 
 /*
+//Random player selection
+function playerPlay() {
+  return choiceArray[Math.floor(Math.random() * choiceArray.length)];
+}
+/*
+//prompt player selection
     function playerPlay() {
       let playerMessage = prompt("Choose wisely between Rock, Paper, Scissors, Lizard & Spock");
       while (choiceArray.includes(playerMessage.toLowerCase()) === false) {
@@ -36,53 +30,53 @@ const scorePlayerArray = [];
 const scoreComputerArray = [];
 const scoreTie = [];
 
-function game() {
+for (let i = 0; i < pressButton.length; i++) {
+  pressButton[i].addEventListener("click", function game() {
+    const playerSelection = pressButton[i].id;
+    console.log("Player: " + playerSelection);
 
-  const playerSelection = playerPlay();
-  console.log("Player: " + playerSelection);
-
-  const computerSelection = computerPlay();
-  console.log("Computer: " + computerSelection);
+    const computerSelection = computerPlay();
+    console.log("Computer: " + computerSelection);
 
     function playRound(playerSelection, computerSelection) {
-        if ((playerSelection === "rock" && (computerSelection === "lizard" || computerSelection === "scissors"))
-          ||
-          (playerSelection === "paper" && (computerSelection === "rock" || computerSelection === "spock"))
-          ||
-          (playerSelection === "scissors" && (computerSelection === "paper" || computerSelection === "lizard"))
-          ||
-          (playerSelection === "lizard" && (computerSelection === "paper" || computerSelection === "spock"))
-          ||
-          (playerSelection === "spock" && (computerSelection === "scissors" || computerSelection === "rock"))){
-          resultBattle = "Player Win";
-          scorePlayerArray.push("1");
-          alert("Player won");
-        } else if (playerSelection === computerSelection) {
-          resultBattle = "Exaequo! That was a close one";
-          scoreTie.push("1");
-          alert("Exaequo");
-        } else {
-          resultBattle = "Computer Win";
-          scoreComputerArray.push("1");
-          alert("Compuer won");
-        };
-     return resultBattle;
-     return scorePlayerArray;
-     return scoreComputerArray;
-     return scoreTie;
-   };
+      if ((playerSelection === "rock" && (computerSelection === "lizard" || computerSelection === "scissors")) ||
+        (playerSelection === "paper" && (computerSelection === "rock" || computerSelection === "spock")) ||
+        (playerSelection === "scissors" && (computerSelection === "paper" || computerSelection === "lizard")) ||
+        (playerSelection === "lizard" && (computerSelection === "paper" || computerSelection === "spock")) ||
+        (playerSelection === "spock" && (computerSelection === "scissors" || computerSelection === "rock"))) {
+        resultBattle = "Player Win";
+        scorePlayerArray.push("1");
+        alert("Player won");
+      } else if (playerSelection === computerSelection) {
+        resultBattle = "Exaequo! That was a close one";
+        scoreTie.push("1");
+        alert("Exaequo");
+      } else {
+        resultBattle = "Computer Win";
+        scoreComputerArray.push("1");
+        alert("Compuer won");
+      };
+      return resultBattle;
+      return scorePlayerArray;
+      return scoreComputerArray;
+      return scoreTie;
+    };
 
-   console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
 
-   console.log("Player score: "+ scorePlayerArray.length);
-   console.log("Computer score: " + scoreComputerArray.length);
-   console.log("Tie score: " + scoreTie.length);
+    console.log("Player score: " + scorePlayerArray.length);
+    console.log("Computer score: " + scoreComputerArray.length);
+    console.log("Tie score: " + scoreTie.length);
 
-   console.log(scorePlayerArray);
-   console.log(scoreComputerArray);
-   document.getElementById("score_player").innerHTML = scorePlayerArray.length;
-   document.getElementById("score_bot").innerHTML = scoreComputerArray.length;
-}
+    console.log(scorePlayerArray);
+    console.log(scoreComputerArray);
+    document.getElementById("score_player").innerHTML = scorePlayerArray.length;
+    document.getElementById("score_bot").innerHTML = scoreComputerArray.length;
+  });
+};
+
+
+
 /*
 //5 games in a row
 for (let i = 0; i < 10; i++) {
